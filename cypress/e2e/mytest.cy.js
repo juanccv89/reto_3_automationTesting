37fa0240c9492362c4cb21f8601b03ccabe4efb1
juanccv89 <134458502+juanccv89@users.noreply.github.com>
2023-08-28 15:25:55 -0500

    Creación de archivo .gitignore.

---
+++ cypress/e2e/mytest.cy.js
@@ -0,0 +1,17 @@
+describe("Filtrado en página de hoteles", () => {
+    beforeEach(() => {
+        cy.visit("www.paradise.com");
+    });
+
+    it("Verifica que todas las cards aparecen en la página desde el inicio", () => {
+        cy.visit
+    });
+
+    it("Aplica cada filtro por precio y verifica que las cards que aparecen corresponden al filtro aplicado", () => {
+        cy.get
+    });
+
+    it("Limpia los filtros y verifica nuevamente que todas las cards estén presentes", () => {
+        cy.get 
+    });
+})
